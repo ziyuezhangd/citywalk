63f1ce0a5e09cd2979f5ff67df9426976def9fcd
daniellez23 <daniellezhzy@gmail.com>
2024-03-06 17:59:28 +0000

    Update Map; Add MapConfig.class

--- src/main/java/groupsix/citywalk/Map.java
+++ src/main/java/groupsix/citywalk/Map.java
@@ -6,9 +6,9 @@ public class Map {
     private final int width;
     private final int height;
     private Location[] bicycleRegion = new Location[2];
-    private HashMap<String, TransportMode> transportList;
-    private HashMap<String, PublicTransportMode> publicList;
-    private HashMap<String, Station> stationList;
+    private HashMap<String, TransportMode> transportList = new HashMap<>();
+    private HashMap<String, PublicTransportMode> publicList = new HashMap<>();
+    private HashMap<String, Station> stationList = new HashMap<>();
 
 
     public Map(int w, int h){
@@ -16,8 +16,8 @@ public class Map {
         height = h;
     }
 
-    //Return the width and height of the map
     public int[] getMap(){
+        //Return the width and height of the map
         int[] shape = new int[2];
         shape[0] = width;
         shape[1] = height;
@@ -28,13 +28,28 @@ public class Map {
         bicycleRegion[0] = l1;
         bicycleRegion[1] = l2;
     }
-
     public Location[] getBicycleRegion(){
         return bicycleRegion;
     }
 
+    public void initStations(){
+        // Initialise all stations on the map
+        MapConfig mapConfig = new MapConfig();
+        for (int i=0; i<mapConfig.stationNames.length; i++){
+            String name = mapConfig.stationNames[i];
+            int x = mapConfig.stationXs[i];
+            int y = mapConfig.stationYs[i];
+            Station station = new Station(name, x, y);
+            stationList.put(name, station);
+        }
 
+    }
+    public void initTransport(){
 
+    }
 
+    public void initPublicTransport(){
+
+    }
 
 }
---
+++ src/main/java/groupsix/citywalk/MapConfig.java
@@ -0,0 +1,9 @@
+package groupsix.citywalk;
+
+
+public class MapConfig {
+    final String[] stationNames = {"UCD", "Trinity", "Dundrum"};
+    final int[] stationXs = {1, 2, 3};
+    final int[] stationYs = {2, 3, 4};
+
+}
