0ce87fd1f59c1c16b42971be45a0b2c9020ba1f9
Skyeeee <boyu.wang01@outlook.com>
2024-03-06 18:41:35 +0000

    Update Score and Player. Create ScoreManager

--- src/main/java/groupsix/citywalk/Player.java
+++ src/main/java/groupsix/citywalk/Player.java
@@ -1,4 +1,81 @@
 package groupsix.citywalk;
+import java.util.List;
 
 public class Player {
+    private String playerName;
+
+    // A player has a Score instance
+    private Score playerScore;
+
+    // The player’s carbon footprint consumption in the current level
+    private int playerFP;
+
+    // The number of gems obtained by the player in the current level
+    private int gemCount;
+
+    // The route chosen by the player in the current level
+    private Route[] selectedRoutes;
+
+    // Time the user has spent in the current level
+    private int time;
+
+    // Store route details
+    private List<RouteDetail> routeDetails;
+    private class RouteDetail {
+        int carbonFP;
+        int transportMode;
+        int time;
+
+        RouteDetail(int carbonFP, int modes, int time) {
+            this.carbonFP = carbonFP;
+            this.transportMode = modes;
+            this.time = time;
+        }
+    }
+
+    // Constructor
+    public Player(String playerName) {
+        this.playerName = playerName;
+        this.playerScore = new Score();
+        this.playerFP = 0;
+        this.gemCount = 0;
+        this.selectedRoutes = new Route[levelCount];
+        for (int i = 0; i < levelCount; i++) {
+            this.selectedRoutes[i] = new Route();
+        }
+        this.time = 0;
+    }
+
+    // Set player name
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    // Update time and footprint
+    public void updateStatus() {
+        // It's related to Level Class
+        // Update time and footprint of user
+        // time = selectedRoutes[level].time;
+        // playerFP = seletedRoutes[level].carbonFP;
+    }
+
+    // Get player's carbon footprint
+    public int getPlayerFP(int playerFP) {
+        return playerFP;
+    }
+
+    // Get the number of gems the player has obtained for the current level
+    public int getPlayerGem() {
+        return gemCount;
+    }
+
+    // Store the routes (trips) taken and for each route
+    // its carbon footprint, the number of modes used, and total travel time
+    public void storeResult() {
+        for (Route route : selectedRoutes) {
+            if (route != null) {
+                routeDetails.add(new RouteDetail(route.getCarbonFP(), route.getModes(), route.getTime()));
+            }
+        }
+    }
 }
--- src/main/java/groupsix/citywalk/Score.java
+++ src/main/java/groupsix/citywalk/Score.java
@@ -1,4 +1,33 @@
 package groupsix.citywalk;
 
 public class Score {
+    private int levelScore;
+    private int scoreSum;
+
+    // Constructor. Initialize level score and total score
+    public Score() {
+        this.levelScore = 0;
+        this.scoreSum = 0;
+    }
+
+    // Calculate the level score based on time and FP
+    public void calScore(int time, int playerFP) {
+        this.levelScore = (int) (Math.log(1000 - (1 * Math.log(time)) - (3 * Math.log(playerFP))) * 10);
+    }
+
+    // Add level score to total score
+    public void updateScores(int levelScore, int scoreSum) {
+        this.scoreSum += levelScore;
+    }
+
+    // Getter for level score
+    public int getLevelScore() {
+        return levelScore;
+    }
+
+    // Getter for total score
+    public int getScoreSum() {
+        return scoreSum;
+    }
+
 }
