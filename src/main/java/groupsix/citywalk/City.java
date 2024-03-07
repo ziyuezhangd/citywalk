caa3d33ca2bedae5e32418f8c44f46b69470ecc5
daniellez23 <daniellezhzy@gmail.com>
2024-03-07 18:11:24 +0000

    Update City and MapConfig

--- src/main/java/groupsix/citywalk/City.java
+++ src/main/java/groupsix/citywalk/City.java
@@ -5,23 +5,25 @@ import java.util.HashMap;
 public class City {
     private final int width;
     private final int height;
-    private static final MapConfig mapConfig = new MapConfig();
     private final Location[] bicycleRegion = new Location[2];
-    private HashMap<String, TransportMode> transportList = new HashMap<>();
-    private HashMap<String, PublicTransportMode> publicList = new HashMap<>();
-    private HashMap<String, Station> stationList = new HashMap<>();
-
-
+    private static HashMap<String, TransportMode> transportList = new HashMap<>();
+    private static HashMap<String, PublicTransportMode> publicList = new HashMap<>();
+    private static HashMap<String, Station> stationList = new HashMap<>();
     public City(){
         //Initialise the city map
-        width = mapConfig.mapSize[0];
-        height = mapConfig.mapSize[1];
+        width = MapConfig.mapSize[0];
+        height = MapConfig.mapSize[1];
         initBicycleRegion();
         initStations();
         initTransport();
         initPublicTransport();
     }
-
+    public static Station getStationByName(String name){
+        return stationList.get(name);
+    }
+    public static TransportMode getTransportByName(String name){
+        return transportList.get(name);
+    }
     public int[] getCitySize(){
         //Return the width and height of the map
         int[] shape = new int[2];
@@ -36,10 +38,10 @@ public class City {
 
     public void initStations(){
         // Initialise all stations on the map
-        for (int i=0; i<mapConfig.stationNames.length; i++){
-            String name = mapConfig.stationNames[i];
-            int x = mapConfig.stationLocs[i][0];
-            int y = mapConfig.stationLocs[i][1];
+        for (int i=0; i<MapConfig.stationNames.length; i++){
+            String name = MapConfig.stationNames[i];
+            int x = MapConfig.stationLocs[i][0];
+            int y = MapConfig.stationLocs[i][1];
             Station station = new Station(name, x, y);
             stationList.put(name, station);
         }
@@ -47,8 +49,8 @@ public class City {
     }
     public void initBicycleRegion(){
         //Initialise the bicycle region on the map
-        bicycleRegion[0] = new Location(mapConfig.bicycleLocs[0][0],mapConfig.bicycleLocs[0][1]);
-        bicycleRegion[1] = new Location(mapConfig.bicycleLocs[1][0],mapConfig.bicycleLocs[1][1]);
+        bicycleRegion[0] = new Location(MapConfig.bicycleLocs[0][0],MapConfig.bicycleLocs[0][1]);
+        bicycleRegion[1] = new Location(MapConfig.bicycleLocs[1][0],MapConfig.bicycleLocs[1][1]);
     }
     public void initTransport(){
 
@@ -57,5 +59,6 @@ public class City {
     public void initPublicTransport(){
 
     }
+
 
 }
--- src/main/java/groupsix/citywalk/MapConfig.java
+++ src/main/java/groupsix/citywalk/MapConfig.java
@@ -1,11 +1,42 @@
 package groupsix.citywalk;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
 
 public class MapConfig {
-    //Define the city map
-    final int[] mapSize = {10,10};
-    final String[] stationNames = {"UCD", "Trinity", "Dundrum"};
-    final int[][] stationLocs = {{1,2}, {3,4}, {5,6}};
-    final int[][] bicycleLocs = {{1,2},{3,4}};
+    //Define the size of city map
+    static final int[] mapSize = {10,10};
+
+    //Define the stations
+    static final String[] stationNames = {"UCD", "Trinity", "Dundrum"};
+    static final int[][] stationLocs = {{1,2}, {3,4}, {5,6}};
+
+    //Define the bicycle region
+    static final int[][] bicycleLocs = {{1,2},{3,4}};
+
+    //Define the public tranport modes
+    static final String[] publicTransportNames = {"Bus", "Luas"};
+    static final HashMap<String,String[]> transportStops;
+    static {
+        transportStops = new HashMap<>();
+        transportStops.put("Bus", new String[]{"UCD", "Dundrum"});
+        transportStops.put("Luas", new String[]{"Trinity", "Dundrum"});
+    }
+
+    //Compute the transport options for each station
+    static final HashMap<String,String[]> transportOptions;
+    static {
+        transportOptions = new HashMap<>();
+        for (String station: stationNames){
+            ArrayList<String> temp_list = new ArrayList<>();
+            for (String transport: publicTransportNames){
+                if (Arrays.asList(transportStops.get(transport)).contains(station)){
+                    temp_list.add(transport);
+                }
+            }
+            String[] temp_array = temp_list.toArray(new String[temp_list.size()]);
+            transportOptions.put(station, temp_array);
+        }
+    }
 }
