052af8b31d114402ee69847afc2aaccb244c2ffc
Yanwen <yu.yanwen.mail@gmail.com>
2024-03-12 16:08:41 +0000

    update Game and level

--- src/main/java/groupsix/citywalk/Game.java
+++ src/main/java/groupsix/citywalk/Game.java
@@ -1,4 +1,36 @@
 package groupsix.citywalk;
 
 public class Game {
+    private Player player;
+    private City city;
+    private int levelCount;
+    private int levelNum = 3;//number of level
+    private int[] levelTime = {30, 20, 10};
+    private int[] levelGem = {1, 2, 3};
+    private int[] levelFP = {30, 20, 10};
+
+    public Game() {
+    }
+    public void startGame(){
+        player = new Player("TestPlayer");
+        city = new City();
+        levelCount = 1;
+    }
+    public void playingGame(){
+        boolean flag = false;
+        for(int i = 0; i < levelNum; i++){
+            Level level = new Level(levelCount, levelTime[i], levelGem[i], levelFP[i], player, city);
+            flag = level.levelPlay();
+            if(flag){
+                levelCount++;
+            }else{
+                break;
+            }
+        }
+        if(flag){
+            System.out.println("Passed!");
+        }else{
+            System.out.println("Failed!");
+        }
+    }
 }
--- src/main/java/groupsix/citywalk/Level.java
+++ src/main/java/groupsix/citywalk/Level.java
@@ -1,4 +1,24 @@
 package groupsix.citywalk;
 
 public class Level {
+    private int levelCount;
+    private int levelTime;
+    private int levelGem;
+    private int levelFP;
+    private Player myPlayer;
+    private City myCity;
+
+    public Level(int levelCount, int levelTime, int levelGem, int levelFP, Player myPlayer, City myCity) {
+        this.levelCount = levelCount;
+        this.levelTime = levelTime;
+        this.levelGem = levelGem;
+        this.levelFP = levelFP;
+        this.myPlayer = myPlayer;
+        this.myCity = myCity;
+    }
+
+    public boolean levelPlay(){
+        System.out.println("This is Level: "+ levelCount);
+        return  true;
+    }
 }
