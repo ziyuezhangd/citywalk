d83a1d08fed828f037e856e58eafca0d9b92fd44
daniellez23 <daniellezhzy@gmail.com>
2024-04-12 11:57:44 +0100

    Update Game scene

--- src/main/java/groupsix/citywalk/api/Controller.java
+++ src/main/java/groupsix/citywalk/api/Controller.java
@@ -1,6 +1,7 @@
 package groupsix.citywalk.api;
 
 import groupsix.citywalk.model.Player;
+import groupsix.citywalk.service.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +9,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -42,25 +46,69 @@ public class Controller {
     private Label levelLabel;
     @FXML
     private GridPane mapGrid;
+    @FXML
+    private ProgressBar timeProgress;
+    @FXML
+    private ProgressBar carbonProgress;
+    @FXML
+    private ProgressBar gemProgress;
+
+    @FXML
     private void switchToGameScene(String playerName) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/groupsix/citywalk/Game.fxml"));
         Parent root = loader.load();
         Controller gameController = loader.getController();
         primaryStage.setScene(new Scene(root));
-
-
+        initGame(playerName);
+    }
+    @FXML
+    private void initGame(String playerName){
+        Game game = new Game();
     }
 
     @FXML
-    private void initMap(){
-        Image grass = new Image("file:pics/grass.png");
-        for (int row = 0; row < 10; row++) {
-            for (int col = 0; col < 10; col++) {
-                ImageView imageView = new ImageView(grass);
-                imageView.setFitWidth(65);
-                imageView.setFitHeight(65);
-                mapGrid.add(imageView, col, row);
-            }
+    private void handleLabelClick(MouseEvent event) {
+        Label chosenLabel = (Label) event.getSource();
+        String stationName = chosenLabel.getId();
+        stationName = stationName.substring(0, stationName.length()-"Label".length());
+        if (!stationName.equals(fromTextField.getText())){
+            toTextField.setText(stationName);
+            generateRoutes();
         }
+    }
+    @FXML
+    private void handleCircleClick(MouseEvent event) {
+        Circle chosenCircle = (Circle) event.getSource();
+        String stationName = chosenCircle.getId();
+        stationName = stationName.substring(0, stationName.length()-"Circle".length());
+        if (!stationName.equals(fromTextField.getText())){
+            toTextField.setText(stationName);
+            generateRoutes();
+        }
+    }
+    @FXML
+    private void generateRoutes() {
+        String startName = fromTextField.getText();
+        String endName = toTextField.getText();
+    }
+    @FXML
+    private void handleResetButton(ActionEvent event) {
+        toTextField.setText(null);
+    }
+    @FXML
+    private void handleStartTrip(ActionEvent event){
+        openEduPopupWindow();
+    }
+
+    //LevelUp View
+
+    //GameOver View
+
+    //GameFinish View
+
+    //Edu Popup View
+    @FXML
+    private void openEduPopupWindow(){
+        //
     }
 }
