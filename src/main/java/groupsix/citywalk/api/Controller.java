08d017fe66126ea022ede8bd91093f9834f68406
daniellez23 <daniellezhzy@gmail.com>
2024-04-12 02:48:11 +0100

    Update game.fxml

--- src/main/java/groupsix/citywalk/api/Controller.java
+++ src/main/java/groupsix/citywalk/api/Controller.java
@@ -7,8 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -31,19 +34,33 @@ public class Controller {
     }
 
     // Game View
+    @FXML
+    private TextField fromTextField;
+    @FXML
+    private TextField toTextField;
+    @FXML
+    private Label levelLabel;
+    @FXML
+    private GridPane mapGrid;
     private void switchToGameScene(String playerName) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/groupsix/citywalk/Game.fxml"));
         Parent root = loader.load();
         Controller gameController = loader.getController();
         primaryStage.setScene(new Scene(root));
+
+
     }
 
-    private void initGame(){
-        //
-    }
-
+    @FXML
     private void initMap(){
-        Image grass = new Image("/groupsix/citywalk/pics/grass.png");
-        
+        Image grass = new Image("file:pics/grass.png");
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                ImageView imageView = new ImageView(grass);
+                imageView.setFitWidth(65);
+                imageView.setFitHeight(65);
+                mapGrid.add(imageView, col, row);
+            }
+        }
     }
 }
