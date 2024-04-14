228448999dcc133a941fe5a50c29558a15f4f91d
Yanwen Yu <146300197+yanwennn@users.noreply.github.com>
2024-04-14 18:50:56 +0100

    Level up scene controller and fxml

---
+++ src/main/java/groupsix/citywalk/maingame/LevelUpController.java
@@ -0,0 +1,37 @@
+package groupsix.citywalk.maingame;
+import groupsix.citywalk.model.Player;
+import groupsix.citywalk.service.Game;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class LevelUpController extends Controller {
+
+    @FXML
+    private Label scoreLabel;
+    @FXML
+    private Label carbonFPLabel;
+
+    @Override
+    @FXML
+    public void nextScene() {
+        try {
+            main.showGameScene();
+        } catch (Exception e) {
+            System.out.println("Error transitioning to the game screen: " + e.getMessage());
+        }
+    }
+
+    public void setUpGame(Game game) {
+        this.game = game;
+        Player player = game.getPlayer();
+        scoreLabel.setText("Your total Score is: " + player.getScoreSum());
+        carbonFPLabel.setText("Your carbon footprint is: " + player.getCarbonFP());
+    }
+
+    @FXML
+    private void switchToSceneGameFinish() {
+//        main.showGameOverScene();
+    }
+
+}
