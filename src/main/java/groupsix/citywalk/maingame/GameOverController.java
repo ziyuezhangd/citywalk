6a8e356e8a6766c494861a1220170713d0e2a168
Yanwen Yu <146300197+yanwennn@users.noreply.github.com>
2024-04-14 19:32:21 +0100

    Update GameOver scene controller and fxml

--- src/main/java/groupsix/citywalk/maingame/GameOverController.java
+++ src/main/java/groupsix/citywalk/maingame/GameOverController.java
@@ -1,5 +1,6 @@
 package groupsix.citywalk.maingame;
 import groupsix.citywalk.model.Player;
+import groupsix.citywalk.service.Game;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 public class GameOverController extends Controller {
     @FXML
     private Label scoreLabel;
+    @FXML
+    private Label carbonFPLabel;
 
     @Override
     public void nextScene() {
@@ -16,5 +19,11 @@ public class GameOverController extends Controller {
             System.out.println("Error transitioning to the score board screen: " + e.getMessage());
         }
 
+    }
+    public void setUpGame(Game game) {
+        this.game = game;
+        Player player = game.getPlayer();
+        scoreLabel.setText("Your total Score is: " + player.getScoreSum());
+        carbonFPLabel.setText("Your carbon footprint is: " + player.getCarbonFP());
     }
 }
